12fe1e037f4e800675404644d690053888027a22
isadora <isadoraalmeidapaixao@gmail.com>
2026-04-02 22:11:40 -0300

    Conceito Herança

---
+++ 01/ControleEstoque/ControleEstoque.API/Models/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.API.Models
+{
+    public enum PerfilUsuario { Cliente, Caixa, Gerente }
+    public abstract class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Nome { get; set; }
+        [Required, EmailAddress, StringLength(100)]
+        public string Email { get; set; }
+        [Required]
+        public string SenhaHash { get; set; }
+        [Required]
+        public PerfilUsuario Perfil { get; set; }
+    }
+
+    public class Cliente : Usuario
+    {
+        [StringLength(14)]
+        public string CPF { get; set; }
+    }
+
+    public class Caixa : Usuario
+    {
+        [StringLength(50)]
+        public string Turno { get; set; }
+    }
+
+    public class Gerente : Usuario
+    {
+        [StringLength(50)]
+        public string Setor { get; set; }
+    }
+}
